9155afb67a5894baf444432a2add3bbd98a68d97
jeredbare <jeredbare@gmail.com>
2017-02-07 21:13:49 -0600

    Create phrasendrescher.sh

---
+++ phrasendrescher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# updates the system
+sudo apt update -y && sudo apt upgrade -y
+# installs dependcies
+sudo apt -y install gcc libssh2-1-dev libssl-dev libgpgme11-dev make
+# downloads the package
+sudo wget http://leidecker.info/projects/phrasendrescher/phrasendrescher-1.2.2b.tar.gz
+# untar the directory
+tar -zxvf phrasendrescher-1.2.2b.tar.gz
+# change to the directory
+cd phrasendrescher-1.2.2b/
+# starts the configuration script
+sudo ./configure --with-plugins
+# make and make install
+sudo make && sudo make install
